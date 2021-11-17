87e29542ff1d323965b236f9b0649d044b176a74
Cristof <cristofor.rotsching@stud.acs.upb.ro>
2021-11-17 14:25:10 +0200

    bounding box coordinates initial definition in generate_edp_from_raster in generate_edm_from_raster.c

--- generate_edm_from_raster.c
+++ generate_edm_from_raster.c
@@ -1,28 +1,35 @@
 #include <stdio.h>
+#include <stdint.h>
 #include "entities.h"
 #define BLACK 0
 #define WHITE 1
 #define SUCCESS 0
 #define FAIL 1
 
+typedef iterator_t size_t
+
 EDP_t *generate_edp_from_pixel(uint8_t raster_pixel, uint8_t raster_pixel_x, uint8_t raster_pixel_y) {
 	EDP_t *edp = (EDP_t *)malloc(1 * sizeof(EDP_t));
 	edp -> luminosity_t = (float) raster_pixel;
-	edp -> pixel.pixel_boundary.P1.x = raster_pixel_x;
-	edp -> pixel.pixel_boundary.P1.y = raster_pixel_y;
-	edp -> pixel.pixel_boundary.P2.x = raster_pixel_x + 1;
-	edp -> pixel.pixel_boundary.P2.y = raster_pixel_y;
-	edp -> pixel.pixel_boundary.P3.x = raster_pixel_x + 1;
-	edp -> pixel.pixel_boundary.P3.y = raster_pixel_y + 1;
-	edp -> pixel.pixel_boundary.P4 = raster_pixel_x;
-	edp -> pixel.pixel_boundary.P4 = raster_pixel_y + 1;
-	edp -> pixel.pixel_coordinates.x = (float) raster_pixe_x;
-	edp -> pixel.pixel_coordinates.y = (float) raster_pixe_y;
+	edp -> pixel.pixel_boundary.P1.x.real_plane_coordinate = raster_pixel_x;
+	edp -> pixel.pixel_boundary.P1.y.real_plane_coordinat  = raster_pixel_y;
+	edp -> pixel.pixel_boundary.P2.x.real_plane_coordinate = raster_pixel_x + 1;
+	edp -> pixel.pixel_boundary.P2.y.real_plane_coordinate = raster_pixel_y;
+	edp -> pixel.pixel_boundary.P3.x.real_plane_coordinate = raster_pixel_x + 1;
+	edp -> pixel.pixel_boundary.P3.y.real_plane_coordinate = raster_pixel_y + 1;
+	edp -> pixel.pixel_boundary.P4.x.real_plane_coordinate = raster_pixel_x;
+	edp -> pixel.pixel_boundary.P4.x.real_plane_coordinate = raster_pixel_y + 1;
+	edp -> pixel.pixel_coordinates.x.real_plane_coordinate = (float) raster_pixel_x;
+	edp -> pixel.pixel_coordinates.y.real_plane_coordinate = (float) raster_pixe_y;
 	return edp;
 }
 
-EDM_t *generate_edm_from_raster(EDP_t* light_sources, size_t light_sources_size) {
-	if (raster_image == NULL) {
+EDM_t *generate_edm_from_edp(EDP_t* light_sources, size_t light_sources_size) {
+	if (light_sources == NULL) {
 	    	return NULL;	
+	}
+	iterator_t it = 0;
+	for (it = 0; i < light_sources_size; it++) {
+		EDP_t edp = light_sources[it];
 	}
 }
